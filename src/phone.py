e1a3e5d414ee5163e98e3f4ce1367b4642cdf4e7
eugene <geymzheka@gmail.com>
2023-11-02 21:44:38 +0500

    added initialization

--- src/phone.py
+++ src/phone.py
@@ -1,1 +1,7 @@
-class Phone:
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
