9fb6ec6d8ee5e005d83c4f98bbe1e2ab984d6fb5
eugene <geymzheka@gmail.com>
2023-11-04 22:27:56 +0500

    added str

--- src/phone.py
+++ src/phone.py
@@ -6,6 +6,9 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
+    def __str__(self):
+        return f'{self.name}'
+
     def __repr__(self):
         return f'{self.__class__.__name__}({self.name}, {self.price}, {self.quantity}, {self.number_of_sim})'
 
