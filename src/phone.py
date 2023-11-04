d2b255348a108d6847af04d76b08dcecd29a9727
eugene <geymzheka@gmail.com>
2023-11-04 18:41:59 +0500

    added method add and repr

--- src/phone.py
+++ src/phone.py
@@ -5,3 +5,10 @@ class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.name}, {self.price}, {self.quantity}, {self.number_of_sim})'
+
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
