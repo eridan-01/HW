8f12036ef236257b59f51401ae39c2150b20bfb3
eugene <geymzheka@gmail.com>
2023-11-04 22:57:50 +0500

    added setter

--- src/phone.py
+++ src/phone.py
@@ -4,7 +4,7 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __str__(self):
         return f'{self.name}'
@@ -16,3 +16,15 @@ class Phone(Item):
         if isinstance(other, Item):
             return self.quantity + other.quantity
         raise TypeError("Складывать можно только объекты классов с родительским классом Item")
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.__number_of_sim = value
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
