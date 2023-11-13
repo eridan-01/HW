4a88ecfb5d2ee421e7e676b918335688486032a0
eugene <geymzheka@gmail.com>
2023-11-13 21:32:59 +0500

    fixed MixinLang

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,20 +3,20 @@ from src.item import Item
 
 class MixinLang:
     def __init__(self):
-        self.__language = 'EN'
+        self.__language = "EN"
+
+    def change_lang(self):
+        self.__language = "RU" if self.__language == "EN" else "EN"
+        return self
 
     @property
     def language(self):
         return self.__language
-
-    def change_lang(self):
-        if self.__language == 'EN':
-            self.__language = 'RU'
-        else:
-            self.__language = 'EU'
 
 
 class Keyboard(Item, MixinLang):
 
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
+        MixinLang.__init__(self)
+
